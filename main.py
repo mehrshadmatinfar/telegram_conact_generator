4bb068f0bbba0869a3f7d868ce7b6f50d9a5c6eb
mehrshadmatinfar <mehrshad.matinfar@modares.ac.ir>
2022-01-20 21:49:16 +0330

    just a copy

---
+++ main.py
@@ -0,0 +1,74 @@
+import requests
+from time import sleep
+
+import json
+from flask import Flask
+from flask import request
+from flask import Response
+
+import os
+
+
+url = "https://api.telegram.org/5129179878:AAHB5eBEXDYFHa9avtbsUD9a6QbrBIz6DPs"
+
+app = Flask(__name__)
+
+
+def get_updates_json(request, offset=None):
+
+    response = requests.get(request + 'getUpdates',
+                            data={'timeout': 100, 'offset': offset})
+
+    return response.json()
+
+
+def last_update(data):
+
+    results = data['result']
+
+    total_updates = len(results) - 1
+
+    return results[total_updates]
+
+
+def get_chat_id(update):
+
+    chat_id = update['message']['chat']['id']
+
+    return chat_id
+
+
+def send_mess(chat, text):
+
+    params = {'chat_id': chat, 'text': text}
+
+    response = requests.post(url + 'sendMessage', data=params)
+
+    return response
+
+
+def sendKirIfKoon(r):
+    msg = r['message']['text']
+    chat_id = r['message']['chat']['id']
+    if msg == 'koon':
+        send_mess(chat_id, 'kir')
+
+
+def write_json(data, fileName='response.json'):
+    with open(fileName, 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+        write_json(msg, '11telegram_request.json')
+        sendKirIfKoon(msg)
+        return Response('ok', status=200)
+    else:
+        return "<h1>salam karbar</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ('PORT', 5000)))
